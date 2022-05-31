dad42e806d81c753e6dbd7888c8716e234f9df50
ductuantruong <ductuanpy99@gmail.com>
2022-05-31 10:44:28 +0800

    change loading upstream model from s3prl to fairseq

--- Model/models.py
+++ Model/models.py
@@ -1,15 +1,16 @@
 import torch
 import torch.nn as nn
+from transformers import Wav2Vec2Model
 
 class Wav2vec2BiEncoder(nn.Module):
     def __init__(self, upstream_model='wav2vec2',num_layers=6, feature_dim=768):
         super().__init__()
-        self.upstream = torch.hub.load('s3prl/s3prl', upstream_model)
+        self.upstream = Wav2Vec2Model.from_pretrained('facebook/wav2vec2-base-960h')
         
         for param in self.upstream.parameters():
             param.requires_grad = True
        
-        for param in self.upstream.model.feature_extractor.conv_layers[:5].parameters():
+        for param in self.upstream.feature_extractor.conv_layers[:5].parameters():
             param.requires_grad = False
         
         encoder_layer_M = torch.nn.TransformerEncoderLayer(d_model=feature_dim, nhead=8, batch_first=True)
@@ -31,8 +32,7 @@ class Wav2vec2BiEncoder(nn.Module):
         )
 
     def forward(self, x, x_len):
-        x = [torch.narrow(wav,0,0,x_len[i]) for (i,wav) in enumerate(x.squeeze(1))]
-        x = self.upstream(x)['last_hidden_state']
+        x = self.upstream(x, output_hidden_states=True).hidden_states[-1]
         xM = self.transformer_encoder_M(x)
         xF = self.transformer_encoder_F(x)
         xM = self.dropout(torch.cat((torch.mean(xM, dim=1), torch.std(xM, dim=1)), dim=1))
