1498799c032e33f2d5a1b4819a8aa6b9c8234d9d
ductuantruong <ductuanpy99@gmail.com>
2022-06-07 18:02:20 +0800

    change downstream to PHO-LID downstream

--- Model/models.py
+++ Model/models.py
@@ -1,28 +1,42 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class Wav2vec2BiEncoder(nn.Module):
     def __init__(self, upstream_model='wav2vec2',num_layers=6, feature_dim=768):
         super().__init__()
         self.upstream = torch.hub.load('s3prl/s3prl', upstream_model)
+        self.feature_dim = feature_dim
+        self.dropout = nn.Dropout(0.1)
         
         for param in self.upstream.parameters():
             param.requires_grad = True
        
         for param in self.upstream.model.feature_extractor.conv_layers[:5].parameters():
             param.requires_grad = False
-        
+
+        self.shared_cnn = nn.Sequential(
+            nn.Dropout(p=0.1),
+            nn.Conv1d(in_channels=feature_dim, out_channels=512, kernel_size=1),
+            nn.ReLU(),
+            nn.BatchNorm1d(512, momentum=0.1, affine=True),
+            nn.Conv1d(in_channels=512, out_channels=512, kernel_size=1),
+            nn.ReLU(),
+            nn.BatchNorm1d(512, momentum=0.1, affine=True),
+            nn.Conv1d(in_channels=512, out_channels=512, kernel_size=1),
+            nn.ReLU(),
+            nn.BatchNorm1d(512, momentum=0.1, affine=True),
+        )
+
         encoder_layer_M = torch.nn.TransformerEncoderLayer(d_model=feature_dim, nhead=8, batch_first=True)
         self.transformer_encoder_M = torch.nn.TransformerEncoder(encoder_layer_M, num_layers=num_layers)
         
         encoder_layer_F = torch.nn.TransformerEncoderLayer(d_model=feature_dim, nhead=8, batch_first=True)
         self.transformer_encoder_F = torch.nn.TransformerEncoder(encoder_layer_F, num_layers=num_layers)
-        
+        self.fc_xv = nn.Linear(1024, feature_dim)
         self.fcM = nn.Linear(2*feature_dim, 1024)
         self.fcF = nn.Linear(2*feature_dim, 1024)
         
-        self.dropout = nn.Dropout(0.5)
-
         self.height_regressor = nn.Linear(1024, 1)
         self.age_regressor = nn.Linear(1024, 1)
         self.gender_classifier = nn.Sequential(
@@ -33,12 +47,20 @@ class Wav2vec2BiEncoder(nn.Module):
     def forward(self, x, x_len):
         x = [torch.narrow(wav,0,0,x_len[i]) for (i,wav) in enumerate(x.squeeze(1))]
         x = self.upstream(x)['last_hidden_state']
+        batch_size = x.size(0)
+        T_len = x.size(1)
+        x = self.dropout(x)
+        x = x.reshape(batch_size * T_len, -1, self.feature_dim).transpose(-1, -2)
+        x = self.shared_cnn(x)
+        stats = torch.cat((x.mean(dim=2), x.std(dim=2)), dim=1)
+        x = self.fc_xv(stats)
+        x = x.view(batch_size, T_len, self.feature_dim)
         xM = self.transformer_encoder_M(x)
         xF = self.transformer_encoder_F(x)
-        xM = self.dropout(torch.cat((torch.mean(xM, dim=1), torch.std(xM, dim=1)), dim=1))
-        xF = self.dropout(torch.cat((torch.mean(xF, dim=1), torch.std(xF, dim=1)), dim=1))
-        xM = self.dropout(self.fcM(xM))
-        xF = self.dropout(self.fcF(xF))
+        xM = torch.cat((torch.mean(xM, dim=1), torch.std(xM, dim=1)), dim=1)
+        xF = torch.cat((torch.mean(xF, dim=1), torch.std(xF, dim=1)), dim=1)
+        xM = F.relu(self.dropout(self.fcM(xM)))
+        xF = F.relu(self.dropout(self.fcF(xF)))
         gender = self.gender_classifier(torch.cat((xM, xF), dim=1))
         output = (1-gender)*xM + gender*xF
         height = self.height_regressor(output)
