78b3a340e08ffc50367b953049aabb3a59a548bc
ductuantruong <ductuanpy99@gmail.com>
2022-07-04 17:47:45 +0800

    weighted sum multi w2v2 feature

--- Model/models.py
+++ Model/models.py
@@ -5,13 +5,16 @@ class Wav2vec2BiEncoder(nn.Module):
     def __init__(self, upstream_model='wav2vec2',num_layers=6, feature_dim=768):
         super().__init__()
         self.upstream = torch.hub.load('s3prl/s3prl', upstream_model)
-        
+        self.n_upstream_encoder = len(self.upstream.model.encoder.layers)
+
         for param in self.upstream.parameters():
             param.requires_grad = True
        
         for param in self.upstream.model.feature_extractor.conv_layers[:5].parameters():
             param.requires_grad = False
-        
+
+        self.encoder_weights = nn.ParameterList([nn.Parameter(torch.randn(1)) for i in range(self.n_upstream_encoder)])
+                
         encoder_layer_M = torch.nn.TransformerEncoderLayer(d_model=feature_dim, nhead=8, batch_first=True)
         self.transformer_encoder_M = torch.nn.TransformerEncoder(encoder_layer_M, num_layers=num_layers)
         
@@ -32,7 +35,16 @@ class Wav2vec2BiEncoder(nn.Module):
 
     def forward(self, x, x_len):
         x = [torch.narrow(wav,0,0,x_len[i]) for (i,wav) in enumerate(x.squeeze(1))]
-        x = self.upstream(x)['last_hidden_state']
+        combined_feature = 0
+        for i in range(self.n_upstream_encoder):
+            while True:
+                try:
+                    hidden_state = self.upstream(x)['hidden_state_{}'.format(i)]
+                    combined_feature += hidden_state * self.encoder_weights[i]
+                    break
+                except:
+                    pass
+        x = combined_feature
         xM = self.transformer_encoder_M(x)
         xF = self.transformer_encoder_F(x)
         xM = self.dropout(torch.cat((torch.mean(xM, dim=1), torch.std(xM, dim=1)), dim=1))
