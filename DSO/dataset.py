52fcb5de4dca83202642b745bda55f1732f8e7ca
ductuantruong <ductuanpy99@gmail.com>
2022-08-23 20:18:30 +0800

    err analysis on DSO

---
+++ DSO/dataset.py
@@ -0,0 +1,57 @@
+from torch.utils.data import Dataset
+import os
+import pandas as pd
+import torch
+import numpy as np
+
+import torchaudio
+import wavencoder
+import random
+
+class DSODataset(Dataset):
+    def __init__(self,
+    wav_folder,
+    language,
+    hparams,
+    ):
+        self.language = language
+        self.wav_folder = wav_folder
+        self.files = os.listdir(self.wav_folder)
+        self.csv_file = hparams.speaker_csv_path
+        self.test_csv_file = hparams.test_speaker_csv_path
+        self.df = pd.read_csv(self.test_csv_file, index_col='ID')
+        self.train_df = pd.read_csv(self.csv_file)
+
+        self.speaker_list = self.df.index.values.tolist()
+        self.gender_dict = {'M' : 0.0, 'F' : 1.0}
+        
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        
+        file = self.files[idx]
+        utt_id = int(file.split('.')[0])
+        id = self.df[(self.df['Language'] == self.language) & (self.df['utt_begin_id'] <= utt_id) & (self.df['utt_end_id'] >= utt_id)].index
+        gender = self.gender_dict[self.df.loc[id, 'Gender'].item()]
+        height = self.df.loc[id, 'Height'].item()
+        age =  self.df.loc[id, 'Age'].item()
+        
+        wav, sr = torchaudio.load(os.path.join(self.wav_folder, file))
+        
+        if(wav.shape[0] != 1):
+            wav = torch.mean(wav, dim=0)
+            
+        wav = torchaudio.transforms.Resample(orig_freq=sr, new_freq=16000)(wav)
+        
+        h_mean = self.train_df[self.train_df['Use'] == 'TRN']['height'].mean()
+        h_std = self.train_df[self.train_df['Use'] == 'TRN']['height'].std()
+        a_mean = self.train_df[self.train_df['Use'] == 'TRN']['age'].mean()
+        a_std = self.train_df[self.train_df['Use'] == 'TRN']['age'].std()
+        
+        height = (height - h_mean)/h_std
+        age = (age - a_mean)/a_std
+        
+        return wav, torch.FloatTensor([height]), torch.FloatTensor([age]), torch.FloatTensor([gender]), id.values.tolist()[0]
---
+++ test_dso.py
@@ -0,0 +1,178 @@
+from config import TIMITConfig
+
+from argparse import ArgumentParser
+from multiprocessing import Pool
+import os
+
+from DSO.dataset import DSODataset
+from TIMIT.lightning_model_uncertainty_loss import LightningModel
+
+from sklearn.metrics import mean_absolute_error, mean_squared_error, accuracy_score, confusion_matrix
+import pytorch_lightning as pl
+
+import torch
+import torch.utils.data as data
+
+from tqdm import tqdm 
+import pandas as pd
+import numpy as np
+
+import torch.nn.utils.rnn as rnn_utils
+def collate_fn(batch):
+    (seq, height, age, gender, speaker_id) = zip(*batch)
+    seql = [x.reshape(-1,) for x in seq]
+    seq_length = [x.shape[0] for x in seql]
+    data = rnn_utils.pad_sequence(seql, batch_first=True, padding_value=0)
+    return data, height, age, gender, seq_length, speaker_id
+
+if __name__ == "__main__":
+
+    parser = ArgumentParser(add_help=True)
+    parser.add_argument('--data_path', type=str, default='/home/project/12001458/ductuan0/speaker_age_height_estimation/data/DSO_data')
+    parser.add_argument('--speaker_csv_path', type=str, default=TIMITConfig.speaker_csv_path)
+    parser.add_argument('--test_speaker_csv_path', type=str, default='/home/project/12001458/ductuan0/speaker_age_height_estimation/DSO/data_info_height_age.csv')
+    parser.add_argument('--batch_size', type=int, default=TIMITConfig.batch_size)
+    parser.add_argument('--epochs', type=int, default=TIMITConfig.epochs)
+    parser.add_argument('--num_layers', type=int, default=TIMITConfig.num_layers)
+    parser.add_argument('--feature_dim', type=int, default=TIMITConfig.feature_dim)
+    parser.add_argument('--lr', type=float, default=TIMITConfig.lr)
+    parser.add_argument('--gpu', type=int, default=TIMITConfig.gpu)
+    parser.add_argument('--n_workers', type=int, default=TIMITConfig.n_workers)
+    parser.add_argument('--dev', type=str, default=False)
+    parser.add_argument('--model_checkpoint', type=str, default=TIMITConfig.model_checkpoint)
+    parser.add_argument('--upstream_model', type=str, default=TIMITConfig.upstream_model)
+    parser.add_argument('--model_type', type=str, default=TIMITConfig.model_type)
+    
+    parser = pl.Trainer.add_argparse_args(parser)
+    hparams = parser.parse_args()
+
+    # Check device
+    if not torch.cuda.is_available():
+        device = 'cpu'
+        hparams.gpu = 0
+    else:        
+        device = 'cuda'
+        print(f'Training Model on TIMIT Dataset\n#Cores = {hparams.n_workers}\t#GPU = {hparams.gpu}')
+    
+    csv_path = hparams.speaker_csv_path
+    test_csv_path = hparams.test_speaker_csv_path
+    df = pd.read_csv(csv_path)
+    test_df = pd.read_csv(test_csv_path)
+    test_df.set_index('ID', inplace=True)
+    h_mean = df[df['Use'] == 'TRN']['height'].mean()
+    h_std = df[df['Use'] == 'TRN']['height'].std()
+    a_mean = df[df['Use'] == 'TRN']['age'].mean()
+    a_std = df[df['Use'] == 'TRN']['age'].std()
+    list_speaker_id = test_df.index.values.tolist()
+
+    #Testing the Model
+    if hparams.model_checkpoint:
+        model = LightningModel.load_from_checkpoint(hparams.model_checkpoint, HPARAMS=vars(hparams))
+        model.to(device)
+        model.eval()
+        list_lang = ['ENGLISH', 'CHINESE']
+        speaker_age_pred_dict = {}
+        speaker_height_pred_dict = {}
+        for speaker_id in list_speaker_id:
+            speaker_age_pred_dict[speaker_id] = []
+            speaker_height_pred_dict[speaker_id] = []
+        list_speaker_id = []
+        for test_lang in list_lang:
+            height_pred = []
+            height_true = []
+            age_pred = []
+            age_true = []
+            gender_pred = []
+            gender_true = []
+            list_idx = []
+            # Testing Dataset
+            test_set = DSODataset(
+                wav_folder = os.path.join(hparams.data_path, test_lang),
+                language = test_lang.capitalize(),
+                hparams = hparams
+            )
+            ## Testing Dataloader
+            testloader = data.DataLoader(
+                test_set, 
+                batch_size=1, 
+                shuffle=False, 
+                num_workers=hparams.n_workers,
+                collate_fn = collate_fn,
+            )
+            for batch in tqdm(testloader):
+                x, y_h, y_a, y_g, x_len, batch_speaker_id = batch
+                x = x.to(device)
+                y_h = torch.stack(y_h).reshape(-1,)
+                y_a = torch.stack(y_a).reshape(-1,)
+                y_g = torch.stack(y_g).reshape(-1,)
+
+                batch_speaker_id = list(batch_speaker_id)
+                for speaker_id in batch_speaker_id:
+                    if speaker_id not in list_speaker_id:
+                        list_speaker_id.append(speaker_id) 
+
+                y_hat_h, y_hat_a, y_hat_g = model(x, x_len)
+                y_hat_h = y_hat_h.to('cpu')
+                y_hat_a = y_hat_a.to('cpu')
+                y_hat_g = y_hat_g.to('cpu')
+                unnormalize_height_pred = (y_hat_h*h_std+h_mean).item()
+                height_pred.append(unnormalize_height_pred)
+                unnormalize_age_pred = (y_hat_a*a_std+a_mean).item()
+                age_pred.append(unnormalize_age_pred)
+                gender_pred.append(y_hat_g>0.5)
+
+                for i, speaker_id in enumerate(batch_speaker_id):
+                    speaker_age_pred_dict[speaker_id].append(unnormalize_age_pred)
+                    speaker_height_pred_dict[speaker_id].append(unnormalize_height_pred)
+                    list_idx.append(speaker_id)
+
+                height_true.append((y_h*h_std+h_mean).item())
+                age_true.append((y_a*a_std+a_mean).item())
+                gender_true.append(y_g[0])
+
+
+            female_idx = np.where(np.array(gender_true) == 1)[0].reshape(-1).tolist()
+            male_idx = np.where(np.array(gender_true) == 0)[0].reshape(-1).tolist()
+
+            height_true = np.array(height_true)
+            height_pred = np.array(height_pred)
+            age_true = np.array(age_true)
+            age_pred = np.array(age_pred)
+
+            hmae = mean_absolute_error(height_true[male_idx], height_pred[male_idx])
+            hrmse = mean_squared_error(height_true[male_idx], height_pred[male_idx], squared=False)
+            amae = mean_absolute_error(age_true[male_idx], age_pred[male_idx])
+            armse = mean_squared_error(age_true[male_idx], age_pred[male_idx], squared=False)
+            print('Test set {}'.format(test_lang))
+            print(hrmse, hmae, armse, amae)
+
+            hmae = mean_absolute_error(height_true[female_idx], height_pred[female_idx])
+            hrmse = mean_squared_error(height_true[female_idx], height_pred[female_idx], squared=False)
+            amae = mean_absolute_error(age_true[female_idx], age_pred[female_idx])
+            armse = mean_squared_error(age_true[female_idx], age_pred[female_idx], squared=False)
+            print(hrmse, hmae, armse, amae)
+            
+            hmae = mean_absolute_error(height_true, height_pred)
+            hrmse = mean_squared_error(height_true, height_pred, squared=False)
+            amae = mean_absolute_error(age_true, age_pred)
+            armse = mean_squared_error(age_true, age_pred, squared=False)
+            print(hrmse, hmae, armse, amae)
+
+            gender_pred_ = [int(pred[0][0] == True) for pred in gender_pred]
+            #print(gender_pred)
+            #print(gender_true)
+            print(accuracy_score(gender_true, gender_pred_))
+            print(confusion_matrix(gender_true, gender_pred_))
+            for i in range(len(gender_pred_)):
+                if gender_pred_[i] != gender_true[i].item():
+                    print(list_idx[i], gender_pred_[i], gender_true[i].item())
+
+        for speaker_id in list_speaker_id:
+            print(speaker_age_pred_dict[speaker_id])
+            speaker_age_pred_dict[speaker_id] = sum(speaker_age_pred_dict[speaker_id])/len(speaker_age_pred_dict[speaker_id])
+            speaker_height_pred_dict[speaker_id] = sum(speaker_height_pred_dict[speaker_id])/len(speaker_height_pred_dict[speaker_id])
+            test_df.at[speaker_id, 'age_prediction'] = round(speaker_age_pred_dict[speaker_id], 2)
+            test_df.at[speaker_id, 'height_prediction'] = round(speaker_height_pred_dict[speaker_id], 2)
+            test_df.to_csv('dso_test.csv')
+    else:
+        print('Model chekpoint not found for Testing !!!')
