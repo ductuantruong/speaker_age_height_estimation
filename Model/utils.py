c6312e07d58e27a4b311d52789aa201316a35a96
ductuantruong <ductuanpy99@gmail.com>
2022-08-29 11:59:06 +0800

    add Mean Squared Log-scaled Error Loss

--- Model/utils.py
+++ Model/utils.py
@@ -16,13 +16,14 @@ class UncertaintyLoss(Module):
     def __init__(self):
         super(UncertaintyLoss, self).__init__()
         self.loss = None
+        self.alpha = 2
         self.loss_height = None
         self.loss_age = None
         self.loss_gender = None
         self.log_var_height = Parameter(torch.tensor(0, requires_grad=True, dtype=torch.float32, device=device))
         self.log_var_age = Parameter(torch.tensor(0, requires_grad=True, dtype=torch.float32, device=device))
         self.log_var_gender = Parameter(torch.tensor(0, requires_grad=True, dtype=torch.float32, device=device))
-
+    
     def forward(self, input, target):
         pred_arr = torch.split(input, input.shape[0]//3)
         height_pred, age_pred, gender_pred = pred_arr
@@ -33,10 +34,12 @@ class UncertaintyLoss(Module):
         self.loss_gender = mse_loss(input=gender_pred, target=gender_target)
         self.loss_gender_var = torch.exp(-self.log_var_gender) * self.loss_gender + self.log_var_gender
         
-        self.loss_height = mse_loss(input=height_pred, target=height_target)
+        self.loss_height = mse_loss(input=height_pred, target=height_target) \
+            + self.alpha * mse_loss(input=torch.add(torch.log(height_pred), 1e-9), target=torch.add(torch.log(height_target), 1e-9))
         self.loss_height_var = torch.exp(-self.log_var_height) * self.loss_height + self.log_var_height
 
-        self.loss_age = mse_loss(input=age_pred, target=age_target)
+        self.loss_age = mse_loss(input=age_pred, target=age_target) + self.alpha \
+            * self.alpha * mse_loss(input=torch.add(torch.log(age_pred), 1e-9) , target=torch.add(torch.log(age_target), 1e-9))
         self.loss_age_var = torch.exp(-self.log_var_age) * self.loss_age + self.log_var_age
 
         self.loss = self.loss_gender + self.loss_height + self.loss_age
