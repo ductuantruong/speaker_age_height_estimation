7f937fbfa18f0bd863ca82c0e46a21eb1ca65ab6
ductuantruong <ductuanpy99@gmail.com>
2022-12-12 21:10:28 +0800

    write pred value to file

--- test_timit.py
+++ test_timit.py
@@ -1,7 +1,6 @@
-from config import ModelConfig
+from config import TIMITConfig
 
 from argparse import ArgumentParser
-from multiprocessing import Pool
 import os
 
 from TIMIT.dataset import TIMITDataset
@@ -19,29 +18,29 @@ import numpy as np
 
 import torch.nn.utils.rnn as rnn_utils
 def collate_fn(batch):
-    (seq, height, age, gender) = zip(*batch)
+    (seq, height, age, gender, speaker_id) = zip(*batch)
     seql = [x.reshape(-1,) for x in seq]
     seq_length = [x.shape[0] for x in seql]
     data = rnn_utils.pad_sequence(seql, batch_first=True, padding_value=0)
-    return data, height, age, gender, seq_length
+    return data, height, age, gender, seq_length, speaker_id
 
 if __name__ == "__main__":
 
     parser = ArgumentParser(add_help=True)
-    parser.add_argument('--data_path', type=str, default=ModelConfig.data_path)
-    parser.add_argument('--speaker_csv_path', type=str, default=ModelConfig.speaker_csv_path)
-    parser.add_argument('--batch_size', type=int, default=ModelConfig.batch_size)
-    parser.add_argument('--epochs', type=int, default=ModelConfig.epochs)
-    parser.add_argument('--num_layers', type=int, default=ModelConfig.num_layers)
-    parser.add_argument('--feature_dim', type=int, default=ModelConfig.feature_dim)
-    parser.add_argument('--lr', type=float, default=ModelConfig.lr)
-    parser.add_argument('--gpu', type=int, default=ModelConfig.gpu)
-    parser.add_argument('--n_workers', type=int, default=ModelConfig.n_workers)
+    parser.add_argument('--data_path', type=str, default=TIMITConfig.data_path)
+    parser.add_argument('--speaker_csv_path', type=str, default=TIMITConfig.speaker_csv_path)
+    parser.add_argument('--batch_size', type=int, default=TIMITConfig.batch_size)
+    parser.add_argument('--epochs', type=int, default=TIMITConfig.epochs)
+    parser.add_argument('--num_layers', type=int, default=TIMITConfig.num_layers)
+    parser.add_argument('--feature_dim', type=int, default=TIMITConfig.feature_dim)
+    parser.add_argument('--lr', type=float, default=TIMITConfig.lr)
+    parser.add_argument('--gpu', type=int, default=TIMITConfig.gpu)
+    parser.add_argument('--n_workers', type=int, default=TIMITConfig.n_workers)
     parser.add_argument('--dev', type=str, default=False)
-    parser.add_argument('--model_checkpoint', type=str, default=ModelConfig.model_checkpoint)
-    parser.add_argument('--upstream_model', type=str, default=ModelConfig.upstream_model)
-    parser.add_argument('--model_type', type=str, default=ModelConfig.model_type)
-    parser.add_argument('--narrow_band', type=str, default=ModelConfig.narrow_band)
+    parser.add_argument('--model_checkpoint', type=str, default=TIMITConfig.model_checkpoint)
+    parser.add_argument('--upstream_model', type=str, default=TIMITConfig.upstream_model)
+    parser.add_argument('--model_type', type=str, default=TIMITConfig.model_type)
+    parser.add_argument('--narrow_band', type=str, default=TIMITConfig.narrow_band)
     
     parser = pl.Trainer.add_argparse_args(parser)
     hparams = parser.parse_args()
@@ -50,10 +49,9 @@ if __name__ == "__main__":
     if not torch.cuda.is_available():
         device = 'cpu'
         hparams.gpu = 0
-        print(f'Testing Model on TIMIT Dataset on CPU')
     else:        
         device = 'cuda'
-        print(f'Testing Model on TIMIT Dataset\n#Cores = {hparams.n_workers}\t#GPU = {hparams.gpu}')
+        print(f'Training Model on TIMIT Dataset\n#Cores = {hparams.n_workers}\t#GPU = {hparams.gpu}')
     
     # Testing Dataset
     test_set = TIMITDataset(
@@ -73,11 +71,13 @@ if __name__ == "__main__":
 
     csv_path = hparams.speaker_csv_path
     df = pd.read_csv(csv_path)
+    df.set_index('ID', inplace=True)
     h_mean = df[df['Use'] == 'TRN']['height'].mean()
     h_std = df[df['Use'] == 'TRN']['height'].std()
     a_mean = df[df['Use'] == 'TRN']['age'].mean()
     a_std = df[df['Use'] == 'TRN']['age'].std()
 
+    list_speaker_id = df[df['Use'] == 'TST'].index.values.tolist()
     #Testing the Model
     if hparams.model_checkpoint:
         model = LightningModel.load_from_checkpoint(hparams.model_checkpoint, HPARAMS=vars(hparams))
@@ -89,26 +89,51 @@ if __name__ == "__main__":
         age_true = []
         gender_pred = []
         gender_true = []
-
+        speaker_age_pred_dict = {}
+        speaker_height_pred_dict = {}
+        
+        for speaker_id in list_speaker_id:
+            speaker_age_pred_dict[speaker_id] = []
+            speaker_height_pred_dict[speaker_id] = []
+        list_speaker_id = []
         for batch in tqdm(testloader):
-            x, y_h, y_a, y_g, x_len = batch
+            x, y_h, y_a, y_g, x_len, batch_speaker_id = batch
             x = x.to(device)
             y_h = torch.stack(y_h).reshape(-1,)
             y_a = torch.stack(y_a).reshape(-1,)
             y_g = torch.stack(y_g).reshape(-1,)
+
+            batch_speaker_id = list(batch_speaker_id)
+            for speaker_id in batch_speaker_id:
+                if speaker_id not in list_speaker_id:
+                    list_speaker_id.append(speaker_id) 
             
             y_hat_h, y_hat_a, y_hat_g = model(x, x_len)
             y_hat_h = y_hat_h.to('cpu')
             y_hat_a = y_hat_a.to('cpu')
             y_hat_g = y_hat_g.to('cpu')
-            height_pred.append((y_hat_h*h_std+h_mean).item())
-            age_pred.append((y_hat_a*a_std+a_mean).item())
+            
+            unnormalize_height_pred = (y_hat_h*h_std+h_mean).item()
+            height_pred.append(unnormalize_height_pred)
+            unnormalize_age_pred = (y_hat_a*a_std+a_mean).item()
+            age_pred.append(unnormalize_age_pred)
             gender_pred.append(y_hat_g>0.5)
 
+            for i, speaker_id in enumerate(batch_speaker_id):
+                speaker_age_pred_dict[speaker_id].append(unnormalize_age_pred)
+                speaker_height_pred_dict[speaker_id].append(unnormalize_height_pred)
+            
             height_true.append((y_h*h_std+h_mean).item())
             age_true.append(( y_a*a_std+a_mean).item())
             gender_true.append(y_g[0])
 
+        for speaker_id in list_speaker_id:
+            speaker_age_pred_dict[speaker_id] = sum(speaker_age_pred_dict[speaker_id])/len(speaker_age_pred_dict[speaker_id])
+            speaker_height_pred_dict[speaker_id] = sum(speaker_height_pred_dict[speaker_id])/len(speaker_height_pred_dict[speaker_id])
+            df.at[speaker_id, 'age_prediction'] = round(speaker_age_pred_dict[speaker_id], 2)
+            df.at[speaker_id, 'height_prediction'] = round(speaker_height_pred_dict[speaker_id], 2)
+        df.to_csv('residual_adapter.csv')
+        
         female_idx = np.where(np.array(gender_true) == 1)[0].reshape(-1).tolist()
         male_idx = np.where(np.array(gender_true) == 0)[0].reshape(-1).tolist()
 
@@ -137,5 +162,7 @@ if __name__ == "__main__":
         
         gender_pred_ = [int(pred[0][0] == True) for pred in gender_pred]
         print(accuracy_score(gender_true, gender_pred_))
+
+
     else:
         print('Model chekpoint not found for Testing !!!')
